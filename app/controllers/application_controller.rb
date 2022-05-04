cca66f3fd8f129525878e3b5f4d6c2131a9fda57
uhikarudevjapan <walrus4l145@icloud.com>
2022-05-04 13:27:21 +0900

    devise導入〜ページ作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :authenticate_user!
+  
+  def configure_permitted_parameters
+    user_params = [:user_name, :email, :password, :password_confirmation, :postal_code, :address, :intro]
+    devise_parameter_sanitizer.permit(:sign_up, keys: user_params)
+    devise_parameter_sanitizer.permit(:account_update, keys: user_params)
+  end
 end
