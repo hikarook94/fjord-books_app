531532cfd01c826d030e0077c7fd5a16fd822765
uhikarudevjapan <walrus4l145@icloud.com>
2022-06-17 15:59:55 +0900

    マイグレーションファイル統合

--- db/migrate/20220601035340_create_reports.rb
+++ db/migrate/20220601035340_create_reports.rb
@@ -3,6 +3,7 @@ class CreateReports < ActiveRecord::Migration[6.1]
     create_table :reports do |t|
       t.string :title, null: false
       t.text :text, null: false
+      t.references :user, null: false, foreign_key: true
 
       t.timestamps
     end
--- db/migrate/20220601050934_add_user_ref_to_report.rb
+++
@@ -1,5 +0,0 @@
-class AddUserRefToReport < ActiveRecord::Migration[6.1]
-  def change
-    add_reference :reports, :user, null: false, foreign_key: true
-  end
-end
